aa0641d0feb191b697a58d751762aeb59101239b
Fadhli Syaifullah <me@fadhli.dev>
2023-11-26 21:46:59 +0700

    23-for-range

---
+++ 23_for_range.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	crusaders := []string{"Avdol", "Kakyouin", "Joutaro", "Joseph", "Polnareff"}
+	for _, crusader := range crusaders {
+		fmt.Println(crusader)
+	}
+}
