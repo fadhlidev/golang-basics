c628b9b7f8f64037815f5d3cc1e325e962f2a9bf
Fadhli Syaifullah <me@fadhli.dev>
2023-11-26 14:43:46 +0700

    02-number-data-type

---
+++ 02_number_data_type.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Integers")
+	fmt.Println(125)
+	fmt.Println(656569)
+
+	fmt.Println("Floating Points")
+	fmt.Println(125.98)
+	fmt.Println(656569.3256)
+}
